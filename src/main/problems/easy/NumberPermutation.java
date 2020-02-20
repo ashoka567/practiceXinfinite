48e3bef3b07f29da730837eb44cc1d3cbdc9d970
Ashok K Gangavarapu <aganagav@cisco.com>
2020-02-20 13:23:49 -0800

    practice on feb 20th

---
+++ src/main/problems/easy/NumberPermutation.java
@@ -0,0 +1,52 @@
+package problems.easy;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class NumberPermutation {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(0, nums, result);
+        return result;
+    }
+
+    private void helper(int start, int[] nums, List<List<Integer>> result){
+        if(start==nums.length-1){
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int num: nums){
+                list.add(num);
+            }
+            result.add(list);
+            return;
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=start; i<nums.length; i++){
+            System.out.println(i + "  -  " +start);
+            if(set.contains(nums[i])){
+                continue;
+            }
+            set.add(nums[i]);
+
+            swap(nums, i, start);
+            helper(start+1, nums, result);
+            swap(nums, i, start);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        NumberPermutation numberPermutation = new NumberPermutation();
+        int[] i = {2,3,4};
+        List<List<Integer>> lists = numberPermutation.permuteUnique(i);
+       lists.forEach(items -> items.forEach(item-> System.out.println(item))) ;
+    }
+}
